2b1b5ebf1bb0c536aae75485d75aeb13efbdb0c3
11 <1511956322@qq.com>
2016-06-22 21:08:19 -0500

    ギリギリ愛 (#6)
    * change transport to json
    
    * change transport to json
    
    * good job!
    
    * merge
    
    * final commit
    
    * mdzz
    
    * mdzz * 2
    
    * add messages type
    
    * ..

--- NewTeach_DAL_Data/FileInfoConvert.cs
+++ NewTeach_DAL_Data/FileInfoConvert.cs
@@ -141,7 +141,11 @@ namespace NewTeach_DAL_Data
 
         static public byte[] ConvertToBytes_FileInfo(FileInfo_mod data)
         {
-            return JsonBytesConvert.ToBytes(ConvertToJson_FileInfo(data));
+<<<<<<< HEAD
+            return JsonBytesConvert.ToBytes(ConvertToJson_FileInfo(data), 2)
+=======
+            return JsonBytesConvert.ToBytes(ConvertToJson_FileInfo(data),2);
+>>>>>>> f6ceabe8aaabd7644130bdf8985438cb6d751287
             /*
             byte[] bResult = new byte[285];
             BitConverter.GetBytes(data.Uid).CopyTo(bResult, 0);
@@ -179,7 +183,7 @@ namespace NewTeach_DAL_Data
 
         static public byte[] ConvertToBytes_Response(FileRequestResponse_mod frr)
         {
-            return JsonBytesConvert.ToBytes(ConvertToJson_Response(frr));
+            return JsonBytesConvert.ToBytes(ConvertToJson_Response(frr), 2)
             /*
             byte[] data = new byte[6];
 
